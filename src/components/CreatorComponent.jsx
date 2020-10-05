8e505cced9661601b54e5fa5194cd1b28bcff3c1
Tarnovskii <artem3dgame@gmail.com>
2020-10-06 00:06:33 +0300

    update ul/bl

--- src/components/CreatorComponent.jsx
+++ src/components/CreatorComponent.jsx
@@ -5,19 +5,38 @@ import s from '../stylesheets/creator.module.css'
 export const Creator = (props) => {
     return (
         <div className={s.wrapper}>
-            <input className={s.title} placeholder={"Название таски"}/>
-            <textarea className={s.description} placeholder={"Описание"}/>
+            <input className={s.title}
+                   onChange={(e) => {
+                       props.formHandler({type: "CHANGE_TITLE", value: e.target.value})
+                   }}
+                   value={props.values.title}
+                   placeholder={"Название таски"}
+            />
+            <textarea className={s.description}
+                      onChange={(e) => {
+                          props.formHandler({type: "CHANGE_DESCRIPTION", value: e.target.value})
+                      }}
+                      value={props.values.description}
+                      placeholder={"Описание"}/>
 
             <p>Начальный статус: </p>
             <div className={s.status_wrapper}>
-                <p><input name="status" type="radio" value="todo"/> To Do</p>
-                <p><input name="status" type="radio" value="inProgress"/> In Progress</p>
-                <p><input name="status" type="radio" value="complete" checked/> Complete</p>
+                <p><input name="status"
+                          onClick={() => props.formHandler({type: "UPDATE_RADIO", value: "todo"})}
+                          type="radio" value="todo" checked={props.values.status === "todo"}/> To Do</p>
+                <p><input name="status"
+                          onClick={() => props.formHandler({type: "UPDATE_RADIO", value: "inProgress"})}
+                          type="radio" value="inProgress" checked={props.values.status === "inProgress"}/> In Progress</p>
+                <p><input name="status"
+                          onClick={() => props.formHandler({type: "UPDATE_RADIO", value: "complete"})}
+                          type="radio" value="complete" checked={props.values.status === "complete"}/> Complete</p>
             </div>
 
-            <p>Дедлайн: <input type={'date'}/></p>
+            <p>Дедлайн: <input value={props.values.date} onChange={(e) => {
+                props.formHandler({type: "CHANGE_DATE", value: e.target.value})
+            }} type={'date'}/></p>
 
-            <button> Создать </button>
+            <button onClick={() => console.log(props.values)}> Создать </button>
         </div>
     )
 }
--- src/containers/TodoCreator.jsx
+++ src/containers/TodoCreator.jsx
@@ -5,11 +5,58 @@ export class TodoCreator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            newTaskFields: {
+                id: 0,
+                title: "",
+                description: "",
+                status: "todo",
+                date: `${new Date().getFullYear()}-${new Date().getMonth() + 1}-0${new Date().getDate()}`,
+                buttonText: ""
+            }
+        }
 
+        this.formHandler.bind(this);
+    }
+
+    formHandler = (action) => {
+        switch (action.type) {
+            case "CHANGE_TITLE":
+                this.setState({
+                    newTaskFields: {
+                        ...this.state.newTaskFields,
+                        title: action.value,
+                    }
+                })
+                break;
+            case "CHANGE_DESCRIPTION":
+                this.setState({
+                    newTaskFields: {
+                        ...this.state.newTaskFields,
+                        description: action.value,
+                    }
+                })
+                break;
+            case "CHANGE_DATE":
+                this.setState({
+                    newTaskFields: {
+                        ...this.state.newTaskFields,
+                        date: action.value,
+                    }
+                })
+                break;
+            case "UPDATE_RADIO":
+                this.setState({
+                    newTaskFields: {
+                        ...this.state.newTaskFields,
+                        status: action.value
+                    }
+                })
+            default:
+                console.log(action);
         }
     }
 
     render() {
-        return <Creator/>
+        return <Creator formHandler={this.formHandler} values={{...this.state.newTaskFields}}/>
     }
 }
